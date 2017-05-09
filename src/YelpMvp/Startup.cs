0a6b722db74d0279d30593ccc13a1ca4a3b9e935
Elise St Hilaire, Jiwon Kang and Minh Phuong <elise+jiwon+minh@>
2017-05-09 10:16:58 -0700

    fix project.json

--- src/YelpMvp/Startup.cs
+++ src/YelpMvp/Startup.cs
@@ -7,15 +7,31 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Texter.Models;
 
-namespace YelpMvp
+namespace Texter
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddEntityFramework()
+                .AddDbContext<TexterContext>(options =>
+                    options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"])); services.AddEntityFramework()
+                 .AddDbContext<TexterContext>(options =>
+                     options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +43,13 @@ namespace YelpMvp
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {
