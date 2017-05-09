621421e1241a059149a83261954719ae8bf9fb1c
Elise St Hilaire, Jiwon Kang and Minh Phuong <elise+jiwon+minh@>
2017-05-09 11:48:21 -0700

    add a Home Controller and Home Index page

---
+++ src/YelpMvp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using YelpMvp.Models;
+
+namespace YelpMvp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetRestaurants()
+        {
+            var allRestaurants = Restaurant.GetRestaurants();
+            return View(allRestaurants);
+        }
+    }
+}
