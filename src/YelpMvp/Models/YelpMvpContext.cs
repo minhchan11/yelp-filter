e9638e0a3f9d2135f9f7470e6c64275f612760d1
Elise St Hilaire, Jiwon Kang and Minh Phuong <elise+jiwon+minh@>
2017-05-09 10:28:48 -0700

    set up Databases in YelpMvpContext

---
+++ src/YelpMvp/Models/YelpMvpContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace YelpMvp.Models
+{
+    public class YelpMvpContext : DbContext
+    {
+        public YelpMvpContext()
+        {
+
+        }
+
+        public virtual DbSet<Location> Locations { get; set; }
+        public virtual DbSet<Cuisine> Cuisines { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=YelpMvp;integrated security=True");
+        }
+
+        public YelpMvpContext(DbContextOptions<YelpMvpContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
