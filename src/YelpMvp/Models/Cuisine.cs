cffc2b5f428aa0f21a9c531e09f57b2a4fce3d21
Elise St Hilaire, Jiwon Kang and Minh Phuong <elise+jiwon+minh@>
2017-05-09 10:36:14 -0700

    add Cuisine and Location Models

---
+++ src/YelpMvp/Models/Cuisine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace YelpMvp.Models
+{
+    [Table("Cuisines")]
+    public class Cuisine
+    {
+        [Key]
+        public int CuisineId { get; set; }
+        public string Name { get; set; }
+    }
+}
